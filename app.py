97edb109756cf8439b3baf328b25c90d88a3072c
jason-Jiong-Long <44335070+jason-Jiong-Long@users.noreply.github.com>
2024-05-22 18:56:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -56,8 +56,8 @@ def callback():
 def handle_message(event):
     msg = event.message.text
     try:
-        #GPT_answer = GPT_response(msg)
-        GPT_answer = msg
+        GPT_answer = GPT_response(msg)
+        #GPT_answer = msg
         print(GPT_answer)
         line_bot_api.reply_message(event.reply_token, TextSendMessage(GPT_answer))
     except:
